e8debb85197cfd7cc9c22d393eebdcc0b2ea821e
Derek Hill <derek.hill@ebench.com>
2019-12-13 15:22:44 +0000

    Way to destroy subscriptions

--- apps/erya/src/app/basic/basic.component.ts
+++ apps/erya/src/app/basic/basic.component.ts
@@ -1,23 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Message } from '@edfu/api-interfaces';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
+import { Subscription } from 'rxjs';
+import { untilDestroyed } from 'ngx-take-until-destroy';
+// import { UntilDestroy } from '@ngneat/until-destroy';
 
+const Hello = gql`
+  query {
+    hello
+  }
+`;
+
+// @UntilDestroy({ checkProperties: true })
+// @UntilDestroy()
 @Component({
   selector: 'edfu-basic',
   templateUrl: './basic.component.html'
 })
-export class BasicComponent implements OnInit {
+export class BasicComponent implements OnInit, OnDestroy {
   hello$ = this.http.get<Message>('/api/hello');
-  message: any;
+  words: any;
   loading = true;
   error: any;
+  hello: any;
+
+  private querySubscription: Subscription;
 
   constructor(private http: HttpClient, private apollo: Apollo) {}
 
   ngOnInit() {
     this.apollo
+
       .watchQuery({
         query: gql`
           {
@@ -31,10 +46,24 @@ export class BasicComponent implements OnInit {
           }
         `
       })
-      .valueChanges.subscribe(result => {
-        this.message = result.data;
+      .valueChanges.pipe(untilDestroyed(this))
+      .subscribe((result: any) => {
+        this.words = result.data.headwordsAll;
         this.loading = result.loading;
         this.error = result.errors;
       });
+
+    this.querySubscription = this.apollo
+      .watchQuery<any>({
+        query: Hello
+      })
+      .valueChanges.subscribe(({ data, loading }) => {
+        this.loading = loading;
+        this.hello = data;
+      });
+  }
+
+  ngOnDestroy() {
+    //   this.querySubscription.unsubscribe();
   }
 }

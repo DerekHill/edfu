985e767c3b5f1a3ad71566289c458b8ca72c7f29
Derek Hill <derek.hill@ebench.com>
2020-01-27 17:05:41 +0000

    Sort of get sense by id

--- apps/erya/src/app/second/second.component.ts
+++ apps/erya/src/app/second/second.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import gql from 'graphql-tag';
 import { Apollo, QueryRef } from 'apollo-angular';
 import { untilDestroyed } from 'ngx-take-until-destroy';
 import { HttpClient } from '@angular/common/http';
-import { HeadwordDto } from '@edfu/api-interfaces';
+import { HeadwordDto, SenseDto } from '@edfu/api-interfaces';
 import { MatOption } from '@angular/material';
 
-interface SearchQuery {
+interface WordSearchQuery {
   search: HeadwordDto[];
 }
 
-interface SearchVariables {
+interface WordSearchVariables {
   search_string?: string;
+}
+
+interface SenseByIdQuery {
+  search: SenseDto[];
+}
+
+interface SenseSearchVariables {
+  senseId: string;
 }
 
 @Component({
@@ -22,15 +30,20 @@ interface SearchVariables {
   templateUrl: './second.component.html'
 })
 export class SecondComponent implements OnInit, OnDestroy {
-  searchFormControl = new FormControl();
-  searchInput: Observable<string>;
-  searchResults$: Observable<HeadwordDto[]>;
-  searchRef: QueryRef<SearchQuery, SearchVariables>;
+  wordSearchFormControl = new FormControl();
+  wordSearchInput: Observable<string>;
+  wordSearchResults$: Observable<HeadwordDto[]>;
+  wordSearchRef: QueryRef<WordSearchQuery, WordSearchVariables>;
+
+  senseSearchResults$: Observable<SenseDto[]>;
+  senseSearchRef: QueryRef<SenseByIdQuery, SenseSearchVariables>;
+
+  tempObservable$: Observable<any>;
 
   constructor(private http: HttpClient, private apollo: Apollo) {}
 
   ngOnInit() {
-    this.searchInput = this.searchFormControl.valueChanges.pipe(
+    this.wordSearchInput = this.wordSearchFormControl.valueChanges.pipe(
       startWith(''),
       map(value => {
         // console.log(value);
@@ -38,7 +51,26 @@ export class SecondComponent implements OnInit, OnDestroy {
       })
     );
 
-    this.searchRef = this.apollo.watchQuery<SearchQuery, SearchVariables>({
+    this.tempObservable$ = of('m_en_gbus0378040.005');
+
+    this.senseSearchRef = this.apollo.watchQuery<
+      SenseByIdQuery,
+      SenseSearchVariables
+    >({
+      query: gql`
+        query Foo($senseId: String!) {
+          sense(senseId: $senseId) {
+            _id
+            example
+          }
+        }
+      `
+    });
+
+    this.wordSearchRef = this.apollo.watchQuery<
+      WordSearchQuery,
+      WordSearchVariables
+    >({
       query: gql`
         query Foo($search_string: String!) {
           search(search_string: $search_string) {
@@ -53,26 +85,49 @@ export class SecondComponent implements OnInit, OnDestroy {
       errorPolicy: 'all'
     });
 
-    this.searchResults$ = this.searchRef.valueChanges.pipe(
+    this.wordSearchResults$ = this.wordSearchRef.valueChanges.pipe(
       map(({ data }: any) => data.search)
     );
 
-    this.searchInput.subscribe(input => {
+    this.senseSearchResults$ = this.senseSearchRef.valueChanges.pipe(
+      map(({ data }: any) => {
+        console.log('senseSearchResults:');
+        console.log(data);
+        return data.search;
+      })
+    );
+
+    this.wordSearchInput.subscribe(input => {
       console.log('input is:');
       if (typeof input === 'string') {
         console.log(input);
-        this.searchRef.setVariables({
+        this.wordSearchRef.setVariables({
           search_string: input
         });
       } else if (typeof input === 'object') {
         console.log(input);
       }
     });
+
+    this.tempObservable$.subscribe(x => {
+      this.senseSearchRef.setVariables({
+        // senseId: input
+        senseId: x
+      });
+      console.log('x');
+      console.log(x);
+    });
+
+    this.senseSearchResults$.subscribe(foo => console.log(foo));
   }
 
   onOptionSelected(input) {
     console.log('onOptionSelected:');
     console.log(input);
+    this.senseSearchRef.setVariables({
+      // senseId: input
+      senseId: 'm_en_gbus0378040.005'
+    });
   }
 
   displayFn(res?: any): string | undefined {

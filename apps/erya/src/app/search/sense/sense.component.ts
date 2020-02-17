14be430dd80c1aeba3e1ee28a7dc93229e5af638
Derek Hill <derek.hill@ebench.com>
2020-02-17 19:29:11 +0000

    Display example or definition

--- apps/erya/src/app/search/sense/sense.component.ts
+++ apps/erya/src/app/search/sense/sense.component.ts
@@ -6,7 +6,19 @@ import { SenseForEntryDtoInterface } from '@edfu/api-interfaces';
   templateUrl: './sense.component.html'
 })
 export class SenseComponent {
-  @Input() sense: SenseForEntryDtoInterface;
+  exampleOrDefinition: string;
+  _sense: SenseForEntryDtoInterface;
+
+  @Input()
+  set sense(sense: SenseForEntryDtoInterface) {
+    this._sense = sense;
+    this.exampleOrDefinition = sense.example || sense.definition;
+  }
+
+  get sense(): SenseForEntryDtoInterface {
+    return this._sense;
+  }
+
   @Output() senseClicked = new EventEmitter<SenseForEntryDtoInterface>();
 
   onSenseClick(event, sense: SenseForEntryDtoInterface) {

68f21fa0784d2297dd11a5748f1be45471047caf
Derek Hill <derek.hill@ebench.com>
2020-02-28 19:15:27 +0000

    Update to process.env.PORT

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const globalPrefix = 'api';
   //   app.enableCors();
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.port || 3333;
+  const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });

f1f48507f5231347c8eaf689475c172dc1d96e2a
Derek Hill <derek.hill@ebench.com>
2020-04-09 15:59:44 +0100

    Switch from watchQuery to query to enable GraphQL caching

--- apps/erya/src/app/graphql.module.ts
+++ apps/erya/src/app/graphql.module.ts
@@ -3,17 +3,6 @@ import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { environment } from '../environments/environment';
-import { DefaultOptions } from 'apollo-client';
-
-// https://www.apollographql.com/docs/react/api/react-apollo/#optionsfetchpolicy
-const defaultOptions: DefaultOptions = {
-  watchQuery: {
-    fetchPolicy: 'cache-first'
-  },
-  query: {
-    fetchPolicy: 'cache-first'
-  }
-};
 
 export function createApollo(httpLink: HttpLink) {
   const http = httpLink.create({
@@ -22,8 +11,7 @@ export function createApollo(httpLink: HttpLink) {
 
   return {
     link: http,
-    cache: new InMemoryCache(),
-    defaultOptions: defaultOptions
+    cache: new InMemoryCache()
   };
 }
 

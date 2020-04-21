628b182073e4c5495032c486a6f1dbd2c28be0d2
Dmitry Lebedev <mityalebedev@me.com>
2020-04-21 19:06:59 +0300

    Move player component into folder

--- apps/erya/src/app/shared/shared.module.ts
+++ apps/erya/src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RemoveUnderscoresPipe } from './pipes/remove-underscores.pipe';
 import { HighlightEntryPipe } from './pipes/highlight-entry.pipe';
 import { OxIdThesaurusPipe } from './pipes/ox-id-thesaurus.pipe';
-import { PlayerComponent } from './components/player.component';
+import { PlayerComponent } from './components/player/player.component';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
